71caa34fc19df83ae7076c7c420d4cfcca6df6cc
Bailey Finkelberg <bfinkel3@jhu.edu>
2022-05-15 23:20:20 -0400

    refactor code

--- jersey_agent.py
+++ jersey_agent.py
@@ -13,12 +13,39 @@ from bs4 import BeautifulSoup
 from urllib import parse, request
 
 options = Options()
-# options.add_argument('--headless')
+options.add_argument('--headless')
+options.add_argument('window-size=1100,700')
 
-# url = 'https://www.nflshop.com/'
-url = 'https://www.fanatics.com/'
-nflshop_searchbox = 'typeahead-input'
-nflshop_searchSubmit = 'typeahead-go'
+url_list = ['https://www.nflshop.com/', 'https://www.fanatics.com/']
+nflshop_searchbox = fanatics_searchbox = 'typeahead-input'
+nflshop_searchSubmit = fanatics_searchSubmit = 'typeahead-go'
+
+def search(search_class, submit_class, name, browser):
+  print("INPUTTING QUERY TO SEARCH BAR")
+  searchbox = browser.find_element(by=By.CLASS_NAME, value=search_class)
+  searchbox.clear()
+  searchbox.send_keys(name)
+
+  print("SEARCHING")
+  browser.find_element(by=By.CLASS_NAME, value=submit_class).click()
+
+def nflShopFilter(size, team, name, browser):
+  print("NARROWING SEARCH")
+  browser.find_element(by=By.PARTIAL_LINK_TEXT, value=(team)).click()
+  browser.find_element(by=By.PARTIAL_LINK_TEXT, value=(size)).click()
+  browser.find_element(by=By.PARTIAL_LINK_TEXT, value=("Jerseys")).click()
+  browser.find_element(by=By.PARTIAL_LINK_TEXT, value=("View all players")).click()
+  browser.find_element(by=By.PARTIAL_LINK_TEXT, value=(name)).click()
+
+def fanaticsLidsFilter(size, team, name, browser):
+  print("NARROWING SEARCH")
+  browser.find_element(by=By.PARTIAL_LINK_TEXT, value=(size)).click()
+  browser.find_element(by=By.PARTIAL_LINK_TEXT, value=("Jerseys")).click()
+  elements = browser.find_elements(by=By.PARTIAL_LINK_TEXT, value = ("NFL"))
+  elements[1].click()
+  browser.find_element(by=By.PARTIAL_LINK_TEXT, value=(team)).click()
+  browser.find_element(by=By.PARTIAL_LINK_TEXT, value=("View all players")).click()
+  browser.find_element(by=By.PARTIAL_LINK_TEXT, value=(name)).click()
 
 # name = input("Enter player name: ")
 # size = input("Men, Women, or Youth Jersey?: ")
@@ -30,70 +57,48 @@ size = "Men"
 color = "Blue"
 team = "Bills"
 
-
 browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 print("INSTALLING DRIVER")
 
-browser.get(url)
-print("URL OPENED")
+for url in url_list:
+  browser.get(url)
+  print("URL OPENED")
 
-time.sleep(5)
-print("WAITING FOR POPUP TO CLOSE")
+  time.sleep(5)
+  print("WAITING FOR POPUP TO CLOSE")
 
-searchbox = browser.find_element(by=By.CLASS_NAME, value=nflshop_searchbox)
-searchbox.clear()
-searchbox.send_keys(name)
-print("QUERY INPUTTED TO SEARCH BAR")
+  # searchbox = browser.find_element(by=By.CLASS_NAME, value=nflshop_searchbox)
+  # searchbox.clear()
+  # searchbox.send_keys(name)
+  # print("QUERY INPUTTED TO SEARCH BAR")
 
-browser.find_element(by=By.CLASS_NAME, value=nflshop_searchSubmit).click()
-print("SEARCHING")
+  # browser.find_element(by=By.CLASS_NAME, value=nflshop_searchSubmit).click()
+  # print("SEARCHING")
 
-# browser.find_element(by=By.PARTIAL_LINK_TEXT, value=(team)).click()
-browser.find_element(by=By.PARTIAL_LINK_TEXT, value=(size)).click()
-browser.find_element(by=By.PARTIAL_LINK_TEXT, value=("Jerseys")).click()
-# browser.find_element(by=By.PARTIAL_LINK_TEXT, value=("NFL")).click()
-elements = browser.find_elements(by=By.PARTIAL_LINK_TEXT, value = ("NFL"))
-elements[1].click()
+  if 'nflshop' in url:
+    search(nflshop_searchbox, nflshop_searchSubmit, name, browser)
+    nflShopFilter(size, team, name, browser)
+  elif 'fanatics' in url or 'lids' in url:
+    search(fanatics_searchbox, fanatics_searchSubmit, name, browser)
+    fanaticsLidsFilter(size, team, name, browser)
 
-browser.find_element(by=By.PARTIAL_LINK_TEXT, value=(team)).click()
-browser.find_element(by=By.PARTIAL_LINK_TEXT, value=("View all players")).click()
-browser.find_element(by=By.PARTIAL_LINK_TEXT, value=(name)).click()
-print("NARROWING SEARCH")
+  item_dict = {}
 
+  try:
+    items = browser.find_elements(by=By.CLASS_NAME, value='product-card-title a')
+    for item in items:
+      itemTitle = item.get_attribute('title')
+      if "Custom" not in itemTitle:
+        itemLink = item.get_attribute('href')
+        item_dict[itemTitle] = [itemLink]
 
+    for k,v in item_dict.items():
+      browser.get(v[0])
+      price_div = browser.find_element(By.CSS_SELECTOR, value = "span[class='sr-only']")
+      price = price_div.get_attribute('innerText')
+      item_dict[k].append(price)
 
-# items = browser.find_elements(by=By.CLASS_NAME, value='product-card-title a')
-# item1 = items[0]
-# itemHTML = item1.get_attribute('title')
-# print(itemHTML)
-
-item_dict = {}
-
-try:
-  items = browser.find_elements(by=By.CLASS_NAME, value='product-card-title a')
-  for item in items:
-    itemTitle = item.get_attribute('title')
-    if "Custom" not in itemTitle:
-      itemLink = item.get_attribute('href')
-      item_dict[itemTitle] = [itemLink]
-
-  print(item_dict)
-  print(len(item_dict))
-
-  for k,v in item_dict.items():
-    browser.get(v[0])
-    price_div = browser.find_element(By.CSS_SELECTOR, value = "span[class='sr-only']")
-    # find_element_by_css_selector("span[class='sr-only']")
-    price = price_div.get_attribute('innerText')
-    item_dict[k] += str(price)
-    # print(price)
-  
-  print(item_dict)
-
-
-  browser.close()
-  browser.quit()
-
-
-except:
-  print("COULD NOT FIND")
+    browser.close()
+    browser.quit()
+  except:
+    print("COULD NOT FIND")
