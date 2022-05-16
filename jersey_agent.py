87345a12379e913d1bf80e59ce7d505f5e4394ef
Bailey Finkelberg <bfinkel3@jhu.edu>
2022-05-15 20:07:27 -0400

    updates

--- jersey_agent.py
+++ jersey_agent.py
@@ -1,6 +1,7 @@
 import selenium
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
@@ -9,19 +10,39 @@ import smtplib, ssl, os
 from email.mime.text import MIMEText
 from email.mime.image import MIMEImage
 from email.mime.multipart import MIMEMultipart
+from bs4 import BeautifulSoup
+
+options = Options()
+options.add_argument('--headless')
+# browser = webdriver.Chrome(CHROMEDRIVER_PATH, chrome_options=options)
 
 url = 'https://www.nflshop.com/'
 nflshop_searchbox = 'typeahead-input'
 nflshop_searchSubmit = 'typeahead-go'
 
-driver_path = '/Users/bailey.finkelberg/Downloads/chromedriver'
-browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
-query = ["Stefon Diggs", "Men's", "Blue"]
+browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+print("INSTALLING DRIVER")
+
+query_terms = ["Stefon Diggs", "Men's", "Blue"]
+
+def formulateQuery(query_terms):
+  query = ''
+  for q in query_terms:
+    query = query + q
+
+  query = query + "Jerseys"
+
+  return query
+
+
+query = formulateQuery(query_terms)
 
 browser.get(url)
+print("URL OPENED")
 
 time.sleep(5)
+print("WAITING FOR POPUP TO CLOSE")
 
 # try:
 #   popButton = browser.find_element_by_xpath('//*[@id="once-per-session___BV_modal_footer_"]/button').click()
@@ -30,25 +51,23 @@ time.sleep(5)
 
 searchbox = browser.find_element(by=By.CLASS_NAME, value=nflshop_searchbox)
 searchbox.clear()
-searchbox.send_keys(query[0])
+searchbox.send_keys(query)
+print("QUERY INPUTTED TO SEARCH BAR")
 
 browser.find_element(by=By.CLASS_NAME, value=nflshop_searchSubmit).click()
+print("SEARCHING")
 
-# items = browser.find_elements(by=By.CLASS_NAME, value='product-card-title')
-# for item in items:
-#   title = item.get_attribute("title")
-#   print("yeah", title)
+items = browser.find_elements(by=By.CLASS_NAME, value='product-card-title a')
+item1 = items[0]
+itemHTML = item1.get_attribute('title')
+print(itemHTML)
 
-# print("still on")
-
-# try:
-#   # items = browser.find_elements(by=By.CLASS_NAME, value='product-card-title')
-#   # print(items)
-
-#   elements = driver.find_elements(by=By.CSS_SELECTOR, value='product-card-title a')
-
-#   print(elements)
-#   # for element in elements:
-#   #     print(element.get_attribute("title"))
-# except:
-#   print("COULD NOT FIND")
+itemTitles = []
+try:
+  items = browser.find_elements(by=By.CLASS_NAME, value='product-card-title a')
+  for item in items:
+    itemTitle = item.get_attribute('title')
+    itemTitles.append(itemTitle)
+  print(itemTitles)
+except:
+  print("COULD NOT FIND")
